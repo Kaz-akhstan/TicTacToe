a7a2a00f357735facc652d1a54bb2d3f9cfe90b6
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-02-28 11:17:08 +0100

    Började om Main

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [][] boardXY = new int[2][n];
-        for (int i = 0; i < n; i++)
+        int turnCounter = 0;
+        char bg = '#';
+        int boardLen = sc.nextInt();
+        char[][] boardXY = new char[boardLen][boardLen];
+        for(int i = 0; i < boardLen; i++)
         {
-            boardXY[0][i] = i;
-            boardXY[1][i] = i;
+            for(int k = 0; k < boardLen; k++)
+            {
+                boardXY[i][k] = bg;
+            }
         }
-        drawGame(n);
+        drawGame(boardLen, boardXY);
+        getInput(sc, boardLen, boardXY);
     }
-    static void drawGame(int len)
+    static void drawGame(int len, char[][] XY)
     {
-        for (int i = 0; i<len; i++)
+        for (int i = 0; i < len; i++)
         {
-            System.out.println();
+            System.out.println(XY[i]);
         }
+    }
+    static void getInput(Scanner sc, int len, char[][] XY)
+    {
+        int input = sc.nextInt();
+        int X = Integer.toString(input).charAt(0);
+        int Y = Integer.toString(input).charAt(1);
+        //Gå till rätt Y Koordinat först, sedan gå till rätt X Koordinat. For loop.
     }
 }
